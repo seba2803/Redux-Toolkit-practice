b66940f1bc89f28be51a7bb99a6ad0e29433a5bd
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-07-27 09:50:45 -0300

    jhrbf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import PostForm from './components/PostForm/PostForm'
-
 import PostsList from './components/PostsList/PostsList'
 
 
 function App() {
 
+ const cele = "kshfkhrwuhnfuw"
+ const lucaa= " putooo";
+ 
   return (
     <div>
       <PostForm/>
